05ca840cccf2918fb563195aba3a4d713bac0f28
dxt420 <37071991+dxt420@users.noreply.github.com>
2020-10-25 16:31:56 +0300

    Update settings

--- project/settings.py
+++ project/settings.py
@@ -137,3 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
